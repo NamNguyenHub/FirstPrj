25380e9f5e73bb67e0843261721527bdbb652b12
NamNguyenHub <19514681.nam@student.iuh.edu.vn>
2024-08-15 20:27:50 +0700

    Commit and Push

--- src/Welcom.java
+++ src/Welcom.java
@@ -3,7 +3,7 @@
 public class Welcom {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello Nam Nguyen ");
+        System.out.println("Hello Nam Nguyen Thanh ");
         System.out.println("Hello to the DHHTTT19A!");
 
     }
